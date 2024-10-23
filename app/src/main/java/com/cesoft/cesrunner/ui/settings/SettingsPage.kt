1789e841e91d8326e497c45c65cbca487d286c73
Cesar Casanova <cesarcl@ext.inditex.com>
2024-10-23 18:02:07 +0200

    Testing a composable number picker

--- app/src/main/java/com/cesoft/cesrunner/ui/settings/SettingsPage.kt
+++ app/src/main/java/com/cesoft/cesrunner/ui/settings/SettingsPage.kt
@@ -1,15 +1,42 @@
 package com.cesoft.cesrunner.ui.settings
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.adidas.mvi.compose.MviScreen
+import com.cesoft.cesrunner.R
+import com.cesoft.cesrunner.ui.common.NumberPicker
 import com.cesoft.cesrunner.ui.settings.mvi.SettingsIntent
-import com.cesoft.cesrunner.ui.settings.mvi.SettingsSideEffect
 import com.cesoft.cesrunner.ui.settings.mvi.SettingsState
+import com.cesoft.cesrunner.ui.theme.SepMax
+import com.cesoft.cesrunner.ui.theme.SepMed
+import com.cesoft.cesrunner.ui.theme.SepMin
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -17,7 +44,7 @@ fun SettingsPage(
     navController: NavController,
     viewModel: SettingsViewModel = koinViewModel(),
 ) {
-    MviScreen<SettingsState, SettingsSideEffect>(
+    MviScreen(
         state = viewModel.state,
         onSideEffect = { sideEffect ->
             viewModel.consumeSideEffect(
@@ -33,13 +60,87 @@ fun SettingsPage(
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Content(
     state: SettingsState,
     reduce: (intent: SettingsIntent) -> Unit,
 ) {
-    Text("Settings")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    //containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(stringResource(R.string.settings))
+                },
+                navigationIcon = {
+                    IconButton(onClick = { reduce(SettingsIntent.Close) }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+//                actions = {
+//                    IconButton(onClick = { /* do something */ }) {
+//                        Icon(
+//                            imageVector = Icons.Filled.Menu,
+//                            contentDescription = "Localized description"
+//                        )
+//                    }
+//                },
+            )
+        },
+    ) { innerPadding ->
+        Surface(modifier = Modifier.padding(innerPadding)) {
+            Settings()
+        }
+    }
+}
+
+@Composable
+private fun Settings() {
+    var period by remember { mutableIntStateOf(5) }
+    LazyColumn(
+        verticalArrangement = Arrangement.Center,
+        //horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = SepMin, horizontal = SepMax)
+    ) {
+        //item { ItemInt(stringResource(R.string.tracks)) }
+        item {
+            Text(stringResource(R.string.period))
+            NumberPicker(period, 1, 60) {
+                android.util.Log.e("AAA", "-------------------- SETTINGS PAGE $period")
+                period = it
+            }
+        }
+    }
+}
+
+@Composable
+private fun SettingsNumberPicker() {
+
+}
+
+@Composable
+private fun ItemInt(
+    title: String,
+    value: Int,
+    onChange: (Int) -> Unit
+) {
+    Column {
+        OutlinedTextField(
+            value = value.toString(),
+            onValueChange = { onChange(it.toInt()) },
+            label = { Text(title) }
+        )
+    }
 }
 
 //--------------------------------------------------------------------------------------------------
--- app/src/main/java/com/cesoft/cesrunner/ui/theme/Metrics.kt
+++ app/src/main/java/com/cesoft/cesrunner/ui/theme/Metrics.kt
@@ -2,4 +2,6 @@ package com.cesoft.cesrunner.ui.theme
 
 import androidx.compose.ui.unit.dp
 
+val SepMin = 5.dp
 val SepMed = 15.dp
+val SepMax = 20.dp
